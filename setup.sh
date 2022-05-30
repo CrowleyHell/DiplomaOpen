3d98af93f281010c59e38d21e6ce717aa80a2ee9
CrowleyHell <n.evvio@bk.ru>
2022-05-30 23:52:26 +0300

    pleaseee

--- setup.sh
+++ setup.sh
@@ -3,11 +3,14 @@
 python3 -m pip install --user virtualenv
 python3 -m venv new_pyenv
 . ./new_pyenv/bin/activate
-pip3 install PyQt5
+apt-get install -y \
+        python3 \
+        pyqt5-dev python3-pyqt5 \
+        python3-psycopg2
+
+apt-get install -y python3-opencv
 pip3 install Pillow
 pip3 install imageio
 pip3 install matplotlib
-pip3 install psycopg2
-pip3 install opencv-python
 pip3 install numpy
 . deactivate
