92dc0161b7592bfb0bf63ac47be67e0b3545f917
CrowleyHell <n.evvio@bk.ru>
2022-05-30 23:42:29 +0300

    notFINALLLasIsaid

--- run.sh
+++ run.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-python3 -m pip install --user virtualenv
-python3 -m venv new_pyenv
 . ./new_pyenv/bin/activate
-pip3 install PyQt5
-pip3 install Pillow
-pip3 install imageio
-pip3 install matplotlib
-pip3 install psycopg2
-pip3 install opencv-python
-pip3 install numpy
 python3 main.py
