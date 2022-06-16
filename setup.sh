a41bbb2b939b8d109f481bdf9ea4b02d5924da11
CrowleyHell <n.evvio@bk.ru>
2022-06-16 11:53:49 +0300

    scriptnew

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-add-apt-repository universe
+add-apt-repository universe -enter
 apt-get update
 apt-get install python3-pip
 apt install python3.9-venv
-python3 -m pip install —user virtualenv
+python3 -m pip install --user virtualenv
 python3 -m venv new_pyenv
 . ./new_pyenv/bin/activate
 apt-get install -y \
@@ -18,11 +18,11 @@ apt-get install postgresql
 apt-get install postgresql postgresql-client
 apt-get install postgresql postgresql-contrib
 apt-get install postgresql-client
-systemctl status postgresql
 pip3 install Pillow
 pip3 install imageio
 pip3 install matplotlib
 pip3 install numpy
+systemctl status postgresql --no-pager
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'devint56';"
 PGPASSWORD=devint56 psql -h localhost -d postgres -U postgres -f base.sql
 deactivate
